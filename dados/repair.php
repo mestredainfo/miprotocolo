c6acc22dea496cb6bb2c3b23c8edf62d8c6f986c
mestredainfo <>
2024-05-20 19:43:37 -0300

    Atualizações gerais do projeto

---
+++ dados/repair.php
@@ -0,0 +1,28 @@
+<?php
+// Copyright (C) 2004-2024 Murilo Gomes Julio
+// SPDX-License-Identifier: GPL-2.0-only
+
+// Organização: Mestre da Info
+// Site: https://linktr.ee/mestreinfo
+
+if (ini_get('zlib.output_compression') == 'Off') {
+    if (extension_loaded('zlib')) {
+        ob_start('ob_gzhandler');
+    }
+}
+
+header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+header("Content-Security-Policy: default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'");
+
+date_default_timezone_set("America/Sao_Paulo");
+
+touch(dirname(__FILE__, 2) . '/update.txt');
+?>
+<script>
+    window.location.assign('/index.php?c=1');
+</script>
