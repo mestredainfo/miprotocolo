7180fc5d7cbe551b4dfcc74df0ee4023219301ae
mestredainfo <>
2024-05-20 16:23:50 -0300

    Correção do atalho

--- criaratalho.php
+++ criaratalho.php
@@ -7,21 +7,21 @@
 
 $sFolder = '/home/' . get_current_user() . '/.local/share/applications/';
 if (file_exists($sFolder)) {
-    $sDesktop = "[Desktop Entry]\n
-Version=" . file_get_contents(dirname(__FILE__) . '/version') . "\n
-Name=MIProtocolo\n
-Comment=Crie e gerencie protocolos para envio e recebimento de documentos\n
-Type=Application\n
-Exec=" . dirname(__FILE__, 4) . "/miprotocolo\n
-Icon=" . dirname(__FILE__, 2) . "/icon/miprotocolo.png\n
-Categories=Utility;";
+    $sDesktop = "[Desktop Entry]
+Version=" . file_get_contents(dirname(__FILE__) . '/version') . "
+Name=MIProtocolo
+Comment=Crie e gerencie protocolos para envio e recebimento de documentos
+Type=Application
+Exec=" . dirname(__FILE__, 4) . "/miprotocolo
+Icon=" . dirname(__FILE__, 2) . "/icon/miprotocolo.png
+Categories=Office;";
 
     $sCreateFile = file_put_contents($sFolder . '/miprotocolo.desktop', $sDesktop);
     if ($sCreateFile) {
-        echo '<script>window.miapp.mensagem(\'MIProtocolo\', \'Atalho criado no menu iniciar!\', \'info\', false);window.location.assign(\'index.php\');</script>';
+        echo '<script>window.miapp.mensagem(\'MIProtocolo\', \'Atalho criado no menu iniciar!\', \'info\', false);window.location.assign(\'index.php?c=1\');</script>';
     } else {
-        echo '<script>window.miapp.mensagem(\'MIProtocolo\', \'Não foi possível criar o atalho no menu iniciar!\', \'error\', false);window.location.assign(\'index.php\');</script>';
+        echo '<script>window.miapp.mensagem(\'MIProtocolo\', \'Não foi possível criar o atalho no menu iniciar!\', \'error\', false);window.location.assign(\'index.php?c=1\');</script>';
     }
 } else {
-    echo '<script>window.miapp.mensagem(\'MIProtocolo\', \'Não foi possível criar o atalho no menu iniciar!\', \'error\', false);window.location.assign(\'index.php\');</script>';
+    echo '<script>window.miapp.mensagem(\'MIProtocolo\', \'Não foi possível criar o atalho no menu iniciar!\', \'error\', false);window.location.assign(\'index.php?c=1\');</script>';
 }
